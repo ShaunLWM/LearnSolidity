2ff9a34f5ca1a8d7e4988ea2e7fe8ae05796c91d
ShaunLWM <shaunlimweimin@gmail.com>
2021-07-10 23:30:44 +0800

    Add simple AuctionHouse contract

---
+++ contracts/AuctionHouse.sol
@@ -0,0 +1,88 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+import "hardhat/console.sol";
+
+contract AuctionHouse is Ownable, ReentrancyGuard {
+    event AuctionCreated(uint256 id);
+    event NewBid(uint256 auctionId, address indexed user, uint256 bidId);
+
+    struct Auction {
+        string auctionName;
+        uint256 timeCreated;
+        uint256 timeStart;
+        uint256 timeEnd;
+        address createdBy;
+        uint256 bidCount;
+        Bid currentHighestBid;
+        mapping(uint256 => Bid) bids;
+    }
+
+    struct Bid {
+        uint256 bidTime;
+        uint256 price;
+        address user;
+    }
+
+    uint256 public currentAuctionId;
+    mapping(uint256 => Auction) public auctions;
+
+    constructor() {}
+
+    function newAuction(
+        string memory auctionName,
+        uint256 timeStart,
+        uint256 timeEnd,
+        uint256 basePrice
+    ) external nonReentrant {
+        require(timeStart < timeEnd, "timeEnd must be more than timeStart");
+        currentAuctionId += 1;
+        Auction storage a = auctions[currentAuctionId];
+        a.auctionName = auctionName;
+        a.timeCreated = block.timestamp;
+        a.timeStart = timeStart;
+        a.timeEnd = timeEnd;
+        a.createdBy = msg.sender;
+        a.bidCount = 0;
+        a.currentHighestBid = Bid({
+            bidTime: 0,
+            price: basePrice,
+            user: msg.sender
+        });
+
+        emit AuctionCreated(currentAuctionId);
+    }
+
+    function addBid(uint256 auctionId, uint256 price) external {
+        require(auctions[auctionId].timeStart == 0, "Auction does not exist");
+        require(
+            block.timestamp >= auctions[auctionId].timeStart,
+            "Auction has not started"
+        );
+
+        require(
+            block.timestamp < auctions[auctionId].timeEnd,
+            "Auction has already ended"
+        );
+
+        require(
+            auctions[auctionId].currentHighestBid.price < price,
+            "Your bid price must be higher than current highest bid price."
+        );
+
+        uint256 bidCount = auctions[auctionId].bidCount;
+        Bid memory newBit = Bid({
+            bidTime: block.timestamp,
+            price: price,
+            user: msg.sender
+        });
+
+        auctions[auctionId].bids[bidCount + 1] = newBit;
+        auctions[auctionId].bidCount += 1;
+        auctions[auctionId].currentHighestBid = newBit;
+        emit NewBid(auctionId, msg.sender, bidCount);
+    }
+}
---
+++ scripts/AuctionHouse.ts
@@ -0,0 +1,17 @@
+import hre from "hardhat";
+
+async function main() {
+	await hre.run("compile");
+
+	const AuctionHouse = await hre.ethers.getContractFactory("AuctionHouse");
+	const auctionHouse = await AuctionHouse.deploy();
+	await auctionHouse.deployed();
+	console.log("AuctionHouse deployed to:", auctionHouse.address);
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
