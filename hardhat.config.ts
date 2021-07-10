67d4918e77f3f8e44cf0dfe02110c07dfa778372
ShaunLWM <shaunlimweimin@gmail.com>
2021-07-10 14:53:30 +0800

    Add Ganache network to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import "@openzeppelin/hardhat-upgrades";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,6 +19,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+	defaultNetwork: "ganache",
+	networks: {
+		ganache: {
+			url: "http://127.0.0.1:7545",
+			accounts: [
+				"a2d64c7b33e066eb6d69fdf3bdc53a19763cdc7ed0e26e42d67be0cfd8a00c91",
+				"8030fd93ab8552037dfc9618c7fc8df53b5dd02f8f7d36015d138f9209110be0",
+			],
+		},
+	},
 	solidity: {
 		version: "0.8.4",
 		settings: {
