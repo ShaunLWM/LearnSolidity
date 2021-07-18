6d9eeed4b8109e625789242218353a828a6d9bab
ShaunLWM <shaunlimweimin@gmail.com>
2021-07-18 23:33:09 +0800

    List all available Auctions

--- client/src/component/AuctionHouse.tsx
+++ client/src/component/AuctionHouse.tsx
@@ -1,11 +1,31 @@
 import { providers as MultiCallProviders } from "@0xsequence/multicall";
-import React, { useEffect } from "react";
+import { Card } from "@mantine/core";
+import { BigNumber, ethers } from "ethers";
+import React, { useEffect, useState } from "react";
 import { useAuctionContract } from "../hooks/useContract";
 import { getMulticallAddress } from "../utils/AddressHelper";
 import { getAuctionContract, simpleRpcProvider } from "../utils/ContractHelper";
 
+interface Bid {
+	bidTime: BigNumber;
+	price: BigNumber;
+	user: string;
+}
+
+interface Auction {
+	auctionName: string;
+	timeCreated: BigNumber;
+	timeStart: BigNumber;
+	timeEnd: BigNumber;
+	createdBy: BigNumber;
+	bidCount: BigNumber;
+	basePrice: BigNumber;
+	currentHighestBid: Bid;
+}
+
 export default function AuctionHouse() {
 	const contract = useAuctionContract();
+	const [auctions, setAuctions] = useState<Auction[]>([]);
 
 	useEffect(() => {
 		const setup = async () => {
@@ -20,8 +40,8 @@ export default function AuctionHouse() {
 				.map((i) => auctionContract.auctions(i));
 
 			try {
-				const multicallRes = await Promise.all(calls);
-				console.log(multicallRes[0]);
+				const contractAuctions = (await Promise.all(calls)) as Auction[];
+				setAuctions(contractAuctions);
 			} catch (error) {
 				console.log(error);
 			}
@@ -30,5 +50,16 @@ export default function AuctionHouse() {
 		setup();
 	}, [contract]);
 
-	return <div></div>;
+	return (
+		<div>
+			{auctions.map((auction) => {
+				return (
+					<Card shadow="sm">
+						<span>{auction.auctionName}</span>
+						<p>HighestBid: {ethers.utils.formatEther(auction.currentHighestBid.price)} by {auction.currentHighestBid.user}</p>
+					</Card>
+				);
+			})}
+		</div>
+	);
 }
