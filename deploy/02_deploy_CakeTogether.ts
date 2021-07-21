eb87de400f06282b38a03122b57f56395fd24ab2
ShaunLWM <shaunlimweimin@gmail.com>
2021-07-21 19:27:09 +0800

    Add CakeTogether deployment with addMinter to xCake

---
+++ deploy/02_deploy_CakeTogether.ts
@@ -0,0 +1,27 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
+import { XCake } from "../typechain/XCake";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+	const { deployments, getNamedAccounts } = hre;
+	const { deploy } = deployments;
+	const { deployer, cakeToken, pcsMasterchef } = await getNamedAccounts();
+
+	const result = await deploy("CakeTogether", {
+		from: deployer,
+		args: [cakeToken, pcsMasterchef],
+		log: true,
+	});
+
+	if (result.newlyDeployed) {
+		const xCakeToken = (await ethers.getContract("xCake", deployer)) as XCake;
+		const tx = await xCakeToken.addMinter(result.address);
+		await tx.wait();
+		console.log(tx)
+	}
+};
+
+export default func;
+func.tags = ["CakeTogether"];
+func.dependencies = ["xCake"];
