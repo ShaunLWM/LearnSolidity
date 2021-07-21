b9a3fa74f80937815fee3585b22451289ddeb3aa
ShaunLWM <shaunlimweimin@gmail.com>
2021-07-21 20:52:32 +0800

    CakeTogether to be deployed with xCake address

--- deploy/02_deploy_CakeTogether.ts
+++ deploy/02_deploy_CakeTogether.ts
@@ -7,15 +7,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	const { deployments, getNamedAccounts } = hre;
 	const { deploy } = deployments;
 	const { deployer, cakeToken, cakeMasterchef } = await getNamedAccounts();
+	const xCakeToken = (await ethers.getContract("xCake", deployer)) as XCake;
 
 	const result = await deploy("CakeTogether", {
 		from: deployer,
-		args: [cakeToken, cakeMasterchef],
+		args: [cakeToken, cakeMasterchef, xCakeToken.address],
 		log: true,
 	});
 
 	if (result.newlyDeployed) {
-		const xCakeToken = (await ethers.getContract("xCake", deployer)) as XCake;
 		const tx = await xCakeToken.addMinter(result.address);
 		await tx.wait();
 		console.log(tx);
