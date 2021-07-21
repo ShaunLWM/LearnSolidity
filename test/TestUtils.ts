133f574513abcc88b499dad5eb43a0bf7f6c3547
ShaunLWM <shaunlimweimin@gmail.com>
2021-07-21 13:24:14 +0800

    Add getBlockTimestamp and getBlockNumber to TestUtils

--- test/TestUtils.ts
+++ test/TestUtils.ts
@@ -1,7 +1,22 @@
-import hre from "hardhat";
+import hre, { ethers } from "hardhat";
 
-async function increaseTime(time: any) {
+const provider = new ethers.providers.JsonRpcProvider();
+
+export async function getBlockNumber() {
+	return await provider.getBlockNumber();
+}
+
+export async function increaseTime(time: any) {
 	let provider = hre.ethers.provider;
 	await provider.send("evm_increaseTime", [time]);
 	await provider.send("evm_mine", []);
+}
+
+export async function getBlockTimestamp() {
+	let block = await provider.getBlock(await getBlockNumber());
+	return block.timestamp;
+}
+
+export async function setBlockTime(ts: number) {
+	await provider.send("evm_setNextBlockTimestamp", [ts]);
 }
