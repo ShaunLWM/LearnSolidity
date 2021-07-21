11929ac25e1235738bdca698c8c054d1b8666f48
ShaunLWM <shaunlimweimin@gmail.com>
2021-07-21 16:50:56 +0800

    Allow Owner to mint xCake

--- contracts/xCake.sol
+++ contracts/xCake.sol
@@ -2,10 +2,15 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract xCake is ERC20 {
+contract xCake is ERC20, Ownable {
     constructor(uint256 initialSupply) ERC20("xCake", "XCK") {
         _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
     function transfer(address recipient, uint256 amount)
